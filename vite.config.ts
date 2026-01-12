bb0dfa96cba39c16d6ab11b490a8c9dbe03fc819
Anthony <nany.rgboy@gmail.com>
2026-01-12 17:56:26 -0500

    Fixed routing for pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/portfolio-website/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/portfolio-website/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
